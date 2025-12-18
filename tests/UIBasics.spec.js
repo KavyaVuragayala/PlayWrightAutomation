08781546341b978ab23557eda35ff51ed6e51986
KavyaVuragayala <vura.kavya18@gmail.com>
2025-12-18 21:08:20 +0530

    Initial Playwright automation project

---
+++ tests/UIBasics.spec.js
@@ -0,0 +1,27 @@
+//const {test} = require('@playwright/test');   
+import { test } from '@playwright/test';     // importing @playwright/test jar 
+
+test('First PlayWright Code',async ()=>
+{
+    //wait
+    //step 1: open browser
+    //wait
+    //step 2 : enter username,password
+    //wait
+    //step 3 : click submit
+ 
+});
+
+test('Browser Context PlayWright', async({browser})=>
+{
+    // async({browser}) ---> {browser} -> declaring the playwright browser 
+    const context = await browser.newContext();  // opens a new browser , we can also pass the cookies etc into newContext();
+    const page = await context.newPage();  // opens a new page 
+    await page.goto("https://www.w3schools.com/")
+
+});
+
+test('Page PlayWright Test', async({page})=>
+{
+    await page.goto("https://www.google.com"); // is there is no cookie or anything to be passed then we can skip context and declare {page}-> this directly open the new page 
+});
